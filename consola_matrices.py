1d10e0d28ab6f0ee3884417c4fe6595cfac08141
andres <andresdavid.ortega@gmail.com>
2022-12-15 07:46:18 -0500

    six commit-determinante

--- consola_matrices.py
+++ consola_matrices.py
@@ -11,8 +11,8 @@ Created on Wed Feb 23 18:10:42 2022
 @author: Andres Ortega
 """
 
-import untitled0 as up
-import determinante_ultimo_2 as du
+import reduccion as up
+import determinante as du
 import copy
 # from tkinter import *
 
@@ -42,83 +42,83 @@ def iniciar():
     while flag:
         
       
-        try:
-            print('menu------------------------------------------------------------------')
-            print('''
-            0. REDUCE A MATRIX
-            1. FIND THE INVERSE
-            2.FIND THE DETERMINANT BY STAGGERED WAY
-            3.INPUT A NEW MATRIX
-            4.EXIT''')
-               
-            o=int(input('''please type one option :'''))
+        # try:
+        print('menu------------------------------------------------------------------')
+        print('''
+        0. REDUCE A MATRIX
+        1. FIND THE INVERSE
+        2.FIND THE DETERMINANT BY STAGGERED WAY
+        3.INPUT A NEW MATRIX
+        4.EXIT''')
+           
+        o=int(input('''please type one option :'''))
+        print()
+    
+
+    
+
+
+
+        if o==0:
+            print('-------------------------SOLUTION---------------------------------')
             print()
-        
-    
-        
-    
-    
-    
-            if o==0:
-                print('-------------------------SOLUTION---------------------------------')
-                print()
-                print()
-                matrix=copy.deepcopy(matriz)
-                print(up.Mostrar(matrix))
-                # text=up.Mostrar(matrix)+'\n'
+            print()
+            matrix=copy.deepcopy(matriz)
+            print(up.Mostrar(matrix))
+            # text=up.Mostrar(matrix)+'\n'
+            # text_area.insert(END,text)
+            try:
+                print(up.redux(matrix, n))
+                # text=up.redux(matrix,n)+'\n'
                 # text_area.insert(END,text)
-                try:
-                    print(up.redux(matrix, n))
-                    # text=up.redux(matrix,n)+'\n'
-                    # text_area.insert(END,text)
-                    # text_area.pack()
-                    # text_area.mainloop()
-                except:
-                    print('!!! THE MATRIX IS NOT INVERTIBLE OR HAVE INFINITE SOLUTIONS ¡¡¡')
-                
+                # text_area.pack()
+                # text_area.mainloop()
+            except:
+                print('!!! THE MATRIX IS NOT INVERTIBLE OR HAVE INFINITE SOLUTIONS ¡¡¡')
             
-            elif o==1:
-                print('-------------------------SOLUTION---------------------------------')
-                matrix=copy.deepcopy(matriz)
-                matrix=up.add_identity(matrix)
-                print()
-                print()
         
-                up.Mostrar(matrix)
-                try:
-                    up.redux(matrix,n)
-                except:
-                    print('!!! THE MATRIX IS NOT INVERTIBLE OR HAVE INFINITE SOLUTIONS ¡¡¡')
-                
-            elif o==2:
-                print('-------------------------SOLUTION---------------------------------')
-                matrix=copy.deepcopy(matriz)
-                du.mean(matrix)
-                
-            elif o==3:
-                print('--------------------------------------------------------------------------')
-                n=int(input("please type the number of rows : "))
-                m=int(input("please type the number of columns :"))
-                print('--------------------------------------------------------------------------')
+        elif o==1:
+            print('-------------------------SOLUTION---------------------------------')
+            matrix=copy.deepcopy(matriz)
+            matrix=up.add_identity(matrix)
+            print()
+            print()
+    
+            print(up.Mostrar(matrix))
+            try:
+                print(up.redux(matrix,n))
+            except:
+                print('!!! THE MATRIX IS NOT INVERTIBLE OR HAVE INFINITE SOLUTIONS ¡¡¡')
+            
+        elif o==2:
+            print('-------------------------SOLUTION---------------------------------')
+            matrix=copy.deepcopy(matriz)
+            print(du.mean(matrix))
+            
+        elif o==3:
+            print('--------------------------------------------------------------------------')
+            n=int(input("please type the number of rows : "))
+            m=int(input("please type the number of columns :"))
+            print('--------------------------------------------------------------------------')
+    
+            matriz=up.create_matrix(n, m) 
+            
+    
+        elif o==4:
+            print('GOOD BYE!-----------------------------------------------------------------')
+            flag=False
         
-                matriz=up.create_matrix(n, m) 
-                
-        
-            elif o==4:
-                print('GOOD BYE!-----------------------------------------------------------------')
-                flag=False
-            
-            else:
-                print()
-                print('PLEASE TYPE A VALID OPTION!!!')
-                print()
-            
-       
-            
-        except:
+        else:
             print()
             print('PLEASE TYPE A VALID OPTION!!!')
             print()
-            pass
+            
+       
+            
+        # except:
+        #     print()
+        #     print('PLEASE TYPE A VALID OPTION!!!')
+        #     print()
+        #     pass
 
 iniciar()
--- determinante.py
+++ determinante.py
@@ -6,43 +6,44 @@ Created on Sun Mar 20 18:03:30 2022
 """
 from fractions import Fraction
 
-import numpy as np
+
+import reduccion as nt
 import copy
 
-def add_lists(list1, list2):
-    new = []
-    for i in range(len(list1)):
-        numero = list1[i]+list2[i]
-        new.append(numero)
-    return new
+# def add_lists(list1, list2):
+#     new = []
+#     for i in range(len(list1)):
+#         numero = list1[i]+list2[i]
+#         new.append(numero)
+#     return new
 
-def multiply_row(row, num):
-    new_row = []
-    for i in range(len(row)):
-        new_num = row[i]*num
-        new_row.append(new_num)
-    return new_row
+# def multiply_row(row, num):
+#     new_row = []
+#     for i in range(len(row)):
+#         new_num = row[i]*num
+#         new_row.append(new_num)
+#     return new_row
 
-def part_list(list1, num):
-    if num != 0:
-        new = []
-        for i in list1:
-            new_num = i/num
-            new.append(new_num)
-    else:
-        new = list1
-    return new
+# def part_list(list1, num):
+#     if num != 0:
+#         new = []
+#         for i in list1:
+#             new_num = i/num
+#             new.append(new_num)
+#     else:
+#         new = list1
+#     return new
 
-def Mostrar(matriz):
-    string='the matrix is as follows :\n'
-    a = np.array(matriz)
+# def Mostrar(matriz):
+#     string='the matrix is as follows :\n'
+#     a = np.array(matriz)
 
-    s = [[str(Fraction(e).limit_denominator()) for e in row] for row in a]
-    lens = [max(map(len, col)) for col in zip(*s)]
-    fmt = '\t'.join('{{:{}}}'.format(x) for x in lens)
-    table = [fmt.format(*row) for row in s]
-    string+='\n'.join(table)+'\n'
-    return string
+#     s = [[str(Fraction(e).limit_denominator()) for e in row] for row in a]
+#     lens = [max(map(len, col)) for col in zip(*s)]
+#     fmt = '\t'.join('{{:{}}}'.format(x) for x in lens)
+#     table = [fmt.format(*row) for row in s]
+#     string+='\n'.join(table)+'\n'
+#     return string
     
 def redux(row,column,matriz):
     string=''
@@ -56,9 +57,9 @@ def redux(row,column,matriz):
     
     
     
-    new_list=multiply_row(matriz[row],num )
-    matriz[column]=add_lists(matriz[column],new_list)
-    return matriz
+    new_list=nt.multiply_row(matriz[row],num )
+    matriz[column]=nt.add_lists(matriz[column],new_list)
+    return matriz,string
 
 def determinant(matriz):
     mult=1
@@ -67,16 +68,27 @@ def determinant(matriz):
     return mult
        
 
-def staggered(i,matriz):
+def staggered(row,matriz):
     
-    column=i+1
-    
+    column=row+1
+    string=''
     while column<len(matriz):
        
-        matriz=redux(i,column,matriz)
+       # matriz,string=redux(i,column,matriz)
+        
+        num=matriz[column][row]*-1*(1/matriz[row][row])
+        if num>0:
+            sign='+'
+            string+='R{2}--->R{2} {3} {1}R{0}'.format(row+1,str(Fraction(num).limit_denominator()),column+1,sign)+'\n'
+        elif num<0:
+            sign=''
+            string+='R{2}--->R{2} {3} {1}R{0}'.format(row+1,str(Fraction(num).limit_denominator()),column+1,sign)+'\n'
+        new_list=nt.multiply_row(matriz[row],num )
+        matriz[column]=nt.add_lists(matriz[column],new_list)
+        
         column+=1
         #Mostrar(matriz)
-    return (matriz)
+    return matriz,string
 
 def show_signs(signs):
     sign=1
@@ -96,6 +108,7 @@ def unique_0(matriz,row,column):
             
 def swap_rows(matriz,row):
     signs=[]
+    string=''
     for i in range(row,len(matriz)-1):
         counter = 1
         
@@ -103,42 +116,43 @@ def swap_rows(matriz,row):
             if unique_0(matriz, i, row):
                 matriz[i], matriz[len(matriz)-1] = matriz[len(matriz)-1], matriz[i]
                 signs.append(-1)
-                print("R{0}<-->R{1}".format(i+1, len(matriz)))
+                string+="R{0}<-->R{1}".format(i+1, len(matriz))+'\n'
                 
             elif matriz[i+counter][row]!=0:
                 matriz[i], matriz[i+counter] = matriz[i+counter], matriz[i]
                 signs.append(-1)
-                print("R{0}<-->R{1}".format(i+1, i+counter+1))
+                string+="R{0}<-->R{1}".format(i+1, i+counter+1)+'\n'
                 
                 
             counter += 1
     
-    print(show_signs(signs))
-    Mostrar(matriz)         
+    string+=str(show_signs(signs))+'\n'
+    string+=nt.Mostrar(matriz)+'\n'         
     
-    
-           
+    return matriz,signs,string
 
-    return matriz,signs
 def mean(matriz):
     signsk=[]
+    strings=''
           
     for i in range(len(matriz)-1):
         if matriz[i][i]==0:
-            matriz,signs=swap_rows(matriz,i)
+            matriz,signs,text=swap_rows(matriz,i)
+            strings+=text
             signsk+=signs
             
             
         if matriz[i][i]!=0:
             compare=copy.deepcopy(matriz)
-            matriz=staggered(i, matriz)
+            matriz,text1=staggered(i, matriz)
+            strings+=text1
             
             
             
         if matriz!=compare:
             
-            print(show_signs(signsk))
-            Mostrar(matriz)
+            strings+=str(show_signs(signsk))+'\n'
+            strings+=nt.Mostrar(matriz)+'\n'
     det=determinant(matriz)*show_signs(signsk)
 
     string='det= '+str(show_signs(signsk))+' x '
@@ -148,8 +162,8 @@ def mean(matriz):
         else:
             string+=str(Fraction(matriz[i][i]).limit_denominator())
     string+=' = '+ str(Fraction(det).limit_denominator())
-    print(string)
+    strings+=string+'\n'
+    
+    return strings
     
     
-
-
